9122dc01b9874247ae52c8b1acdd6f1a8bd5f78a
NAGASAKI-1 <gpiras.5608@gmail.com>
2025-10-19 16:18:10 +0200

    aggiunto file di funzioni js con implementazione di sasso carta forbici e file di prova

---
+++ codice/js/funzioni.js
@@ -0,0 +1,13 @@
+function saCaFo(input) {
+  let computer = Math.floor(Math.random() * 3)
+  console.log(computer);
+
+  if (computer == input)
+    alert('pari');
+
+  if (computer == 1 && input == 0 || computer == 0 && input == 1 || computer == 0 && input == 2)
+    alert('computer');
+
+  if (computer == 2 && input == 0 || computer == 2 && input == 1 || computer == 1 && input == 2)
+    alert('giocatore');
+}
