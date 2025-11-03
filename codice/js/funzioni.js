d9b7c403cea3b6d23b3f272d6c3104877c53d1e2
NAGASAKI-1 <gpiras.5608@gmail.com>
2025-11-03 18:45:27 +0100

    commit pre merge

--- codice/js/funzioni.js
+++ codice/js/funzioni.js
@@ -16,7 +16,20 @@ function inizia () {
 //sessionStorage.setItem serve a memorizzre un dato fino a che non viene chiusa la finestra di browser, quindi un dato rimane memorizzato per tutta la sessione di gioco
 //ci serve per memorizzare cose come se sono state prese le medicine, se è stata vinta una battaglia
 
-//BOILERPLATE!!!!!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function disegnaVite(ViteId, numeroVite) {
   //ViteId sarebbe che tipo di vite sono, quindi se si parla di vite del giocatore o del Boss
@@ -46,6 +59,7 @@ function disegnaVite(ViteId, numeroVite) {
   }
 }*/
 
+
 function saCaFo(input, urlSeVince, urlSePerde) {
   let computer = Math.floor(Math.random() * 3)
   console.log(computer);
@@ -53,18 +67,28 @@ function saCaFo(input, urlSeVince, urlSePerde) {
   if (computer == input){
     alert('pari');
   }
+
+  //se vince il computer
   if (computer == 1 && input == 0 || computer == 0 && input == 1 || computer == 0 && input == 2){
     alert('computer');
     viteGiocatore--;
-    if (viteGiocatore === 0)
+    if (viteGiocatore === 0){
+      if (document.body.id === 'porta')
+        sessionStorage.setItem("vintoBossPorta", false);
       apri(urlSePerde);
+    }
     disegnaVite("viteGiocatore", viteGiocatore);
   }
+
+  //se vince il giocatore
   if (computer == 2 && input == 0 || computer == 2 && input == 1 || computer == 1 && input == 2){
     alert('giocatore');
     viteComputer--;
-    if (viteComputer === 0)
+    if (viteComputer === 0){
+      if (document.body.id === 'porta')
+        sessionStorage.setItem("vintoBossPorta", true);
       apri(urlSeVince);
+    }
     disegnaVite("viteComputer", viteComputer);
   }
 
@@ -87,6 +111,8 @@ function creaBottoneCorridoio(){
   document.getElementById("bottoneVittoriaPorta").appendChild(bottone);
 }
 
+
+//apre la nuova pagina applicando un effetto di transizione (fade-out)
 function apri(pagina){
   let overlay = document.getElementById('overlay');
   overlay.classList.remove('visible');
@@ -98,7 +124,6 @@ function apri(pagina){
 //
 
 function prendiMedicine(){
-  sessionStorage.removeItem('preseMedicine');
   sessionStorage.setItem('preseMedicine', true);
 }
 
@@ -106,10 +131,9 @@ function prendiMedicine(){
 
 function creaBottoneBossCereali(){
   if(!preseMedicine){
-    let bottone = document.createElement("input");
+    let bottone = document.createElement("button");
 
-    bottone.type = "button";
-    bottone.value = "Fai colazione";
+    bottone.innerHTML = "Fai colazione";
     bottone.id = "bossCereali";
     bottone.classList.add("bottone"); 
     bottone.onclick = function(){
@@ -120,35 +144,70 @@ function creaBottoneBossCereali(){
   }
 }
 
+function loadCamera(){
+
+  //se non si ha ancora giocato contro la porta carica il bottone 'porta'
+  if(!sessionStorage.getItem('giocatoControPorta')){
+    let bottone = document.createElement("button");
+    bottone.innerHTML = "Porta";
+    bottone.id = "portaCamera";
+    bottone.classList.add("bottone"); 
+    bottone.onclick = "apri('Boss_Porta.html')";
+    document.body.appendChild(bottone);
+  }
+
+  //se si ha giocato contro la porta e si ha vinto
+  if(sessionStorage.getItem('giocatoControPorta') && sessionStorage.getItem('vintoBossPorta')){
+    let bottone = document.createElement("button");
+    bottone.innerHTML = "Porta";
+    bottone.id = "portaCamera";
+    bottone.classList.add("bottone"); 
+    bottone.onclick = "apri('Corridoio.html')";
+    document.body.appendChild(bottone);
+  }
+
+}
+
+function loadPorta(){
+  disegnaVite("viteGiocatore", viteGiocatore);
+  disegnaVite("viteComputer", viteComputer);
+  sessionStorage.setItem("giocatoControPorta", true);
+}
+
 
 
 /*****************EVENT LISTENERS***************/
 
 addEventListener('load', () => {
-  let overlay = document.getElementById('overlay');
-  overlay.classList.add('visible');
+  
+  //identifica la carica e ne carica dinamicamente alcuni elementi
   switch (document.body.id) 
   {
-    case "cucina":
-      creaBottoneBossCereali();
-      break;
+    /*case "cucina":
+      loadCucina();
+      break;*/
 
     case "porta":
-      if (!sessionStorage.getItem("giocatoControPorta")) {
-        disegnaVite("viteGiocatore", viteGiocatore);
-        disegnaVite("viteComputer", viteComputer);
-        sessionStorage.setItem("giocatoControPorta", true);
-      }
-      else
-        creaBottoneCorridoio();
+      loadPorta();
       break;
 
-    case "dio":
+    /*case "dio":
+      loadDio();
       disegnaVite("viteGiocatore", viteGiocatore);
       disegnaVite("viteComputer", viteComputer);
+      break;*/
+    
+    case "camera":
+      loadCamera();
       break;
 
   }
+
+
+  //transizione tra le pagine (fade-in)
+  let overlay = document.getElementById('overlay');
+  overlay.classList.add('visible');
+
 })
 
 //
